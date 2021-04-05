33542fb91546f0cf92cf5722d55345d90249610c
Abhishek Verma <57979897+abhishekverma1997@users.noreply.github.com>
2021-04-05 11:42:24 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 @app.route('/wtf')
 def home():
-	return render_template('index2.html')
+	return render_template('pitt_nivesh_home.html')
 
 @app.route('/user_reg', methods=['POST'])
 def user_registration():
@@ -47,8 +47,78 @@ def user_registration():
         con.close()
             
         return render_template('index2.html')
-            
+ 
 
+@app.route('/home_page')
+def user_home_page():
+    return render_template('pitt_nivesh_home.html')
+
+@app.route('/log_out_to_user_reg')
+def log_out_to_user_reg():
+    return render_template('index2.html')
+
+@app.route('/login_user')
+def login_user():
+    return render_template('login_auth.html')
+
+
+@app.route('/user_authentication',methods=['POST'])
+def user_authentication():
+    if request.method == 'POST':
+        email_id = request.form['email']
+        password = request.form['password']
+        
+        con = pymysql.connect(host='134.209.169.96',
+                             user='pitt_nivesh',
+                             password='pitt_Nivesh_123@!',
+                             db='pitt_nivesh',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        
+        cursor = con.cursor()
+        
+        qry = 'SELECT * FROM user_login WHERE '
+        qry = qry + 'email LIKE %s AND password LIKE %s '
+        
+        cursor.execute(qry, (email_id, password)) 
+    
+        rows = cursor.fetchall()
+        #con.commit()
+        #con.close()
+        
+        print(len(rows))
+        
+        #print(rows[0]['email'])
+        
+        con.commit()
+        con.close()
+        
+        #return render_template('login_auth.html', what=rows)
+        
+        #return render_template('login_auth.html', what="USER DOES NOT EXIST")
+        
+        if(len(rows)==0):
+            
+            return render_template('login_auth.html', what= "USER DOES NOT EXIST" )
+        
+        if(rows[0]['email']==email_id and rows[0]['password']==password):
+            
+            return render_template('login_auth.html', what=rows)
+        
+        else:
+            return render_template('login_auth.html', what= "USER DOES NOT EXIST" )
+            
+        
+        
+    
+    
+        
+
+@app.route('/stock_search')
+def stock_search_button():
+    return render_template('stocks.html')
+    
+           
 @app.route('/stock', methods=['POST'])
 def stock_search():
     if request.method == 'POST':
@@ -72,12 +142,12 @@ def stock_search():
         rows = cursor.fetchall()
         
         con.commit()
+        
         con.close()
         
-        print(rows)
         
         #render_template_string('hello {{ what }}', what=rows)
-        return render_template_string('{{ what }}', what=rows)
+        return render_template('stocks.html', what = rows)
         
         
         
